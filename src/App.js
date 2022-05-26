ef327f4056dff14c780d2fa69bb449c8602857b1
Rasool97 <raybodjazini97@gmail.com>
2022-05-26 12:22:38 +0430

    Edit routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = () => {
     return (
         <Switch>
             <Route path='/' exact component={Home} />
+            <Redirect from='/*' to='/' />
         </Switch>
     );
 };
